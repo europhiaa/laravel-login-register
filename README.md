8159a5abb7adbfc485c516cffcc2e03571b4c982
europhia <europhia.lancer@gmail.com>
2023-06-12 18:23:46 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## Template
+https://github.com/ColorlibHQ/AdminLTE/releases/tag/v3.1.0
+
+cp .env.example .env
+
+## Langkah
+- php artisan migrate
+- php artisan make:seeder UserData
+- php artisan db:seed --class=UserData
+- php artisan make:middleware CekUserLogin
+
+### update kernel.php di bagian protected $routeMiddleware
+'cekUserLogin' => \App\Http\Middleware\CekUserLogin::class,
+
+- php artisan make:controller LoginController
+- php artisan make:controller BerandaController
+- php artisan make:controller KasirController
---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function index(){
+        if($user = Auth::user()){
+            if($user->level == '1'){
+                return redirect()->intended('beranda');
+            } elseif ($user->level == '2'){
+                return redirect()->intended('kasir');
+            }
+        }
+
+        return view('login.view_login');
+    }
+
+    public function proses(Request $request){
+        $request->validate([
+            'username' => 'required',
+            'password' => 'required'
+        ],
+        [
+            'username.required' => 'Username tidak boleh kosong',
+        ]
+        );
+
+        $kredensial = $request->only('username','password');
+        if(Auth::attempt($kredensial)){
+        $request->sesion()->regenerate();
+            $user = Auth::user();
+            if($user->level == '1'){
+                return redirect()->intended('beranda');
+            } elseif ($user->level == '2'){
+                return redirect()->intended('kasir');
+            }
+
+            return redirect()->intended('/');
+        }
+
+        return back()->withErrors([
+            'username' => 'Username atau password salah'
+        ])->onlyInput('username');
+    }
+}
